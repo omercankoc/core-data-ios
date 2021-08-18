61a1ce6397a80aeb178c2cc1bf29ace780a54104
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-08-18 19:41:27 +0300

    READ_EXTERNAL_STORAGE

--- ContentProviderSQL/app/src/main/java/com/omercankoc/contentprovidersql/DetailActivity.kt
+++ ContentProviderSQL/app/src/main/java/com/omercankoc/contentprovidersql/DetailActivity.kt
@@ -1,11 +1,18 @@
 package com.omercankoc.contentprovidersql
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlin.Exception
 
 class DetailActivity : AppCompatActivity() {
 
@@ -27,7 +34,44 @@ class DetailActivity : AppCompatActivity() {
     }
 
     fun select(view : View){
+        // Galeriye erismek icin izin yok ise izin iste.
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+        }
+        // Galeriye erismek icin izin var ise galeriye yonlendir.
+        else {
+            val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent,1)
+        }
+    }
 
+    // Izin verildikten sonra galeriye yonlendir.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == 1){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent,2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    // Kullanicinin galeriden sectigi image'i ilgili nesneye aktar.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == 2 && resultCode == RESULT_OK && data != null){
+            val uri = data.data
+            try{
+                val image = MediaStore.Images.Media.getBitmap(this.contentResolver,uri)
+                imageViewLanguage.setImageBitmap(image)
+            } catch (e : Exception){
+                e.printStackTrace()
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     fun save(view : View){
@@ -39,6 +83,6 @@ class DetailActivity : AppCompatActivity() {
     }
 
     fun delete(view : View){
-        
+
     }
 }
