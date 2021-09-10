1985c90c6c0bdb57e7c4a75a8d8de3096d804eca
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-09-10 23:53:40 +0300

    Created

---
+++ CoreDataiOS/CoreDataiOS/DetailsViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageViewImage: UIImageView!
+    @IBOutlet weak var textFieldLanguage: UITextField!
+    @IBOutlet weak var textFieldDeveloper: UITextField!
+    @IBOutlet weak var textFieldYear: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        
+    }
+}
---
+++ CoreDataiOS/CoreDataiOS/ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableViewLanguages: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
