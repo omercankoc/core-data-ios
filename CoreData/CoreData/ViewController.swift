3941f57ebcb9e8bbb3a900e3b0aff6600c1c2d9d
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-09-10 18:35:46 +0300

    Created Sample!

---
+++ CoreData/CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Ömer Can Koç on 10.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
